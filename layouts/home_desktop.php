ee2a8273a28a94dcb02d245a8e247d57a9213c04
Adriribra11 <adrirbra10@gmail.com>
2023-11-03 11:10:53 +0100

    editing homes

---
+++ layouts/home_desktop.php
@@ -0,0 +1,412 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="initial-scale=1, width=device-width" />
+
+    <link rel="stylesheet" href="./global.css" />
+    <link rel="stylesheet" href="./index.css" />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Inter:wght@400&display=swap"
+    />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Roboto Condensed:wght@700&display=swap"
+    />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Signika Negative:wght@400;700&display=swap"
+    />
+  </head>
+  <body>
+    <div class="home-desktop">
+      <div class="inicio">
+        <div class="header">
+          <div class="header1">
+            <div class="user-con-fondo-parent">
+              <img
+                class="user-con-fondo-icon"
+                alt=""
+                src="./public/userconfondo.svg"
+              />
+
+              <div class="usuario">Usuario</div>
+            </div>
+            <div class="menu">
+              <div class="item1">
+                <div class="peliculas">Peliculas</div>
+              </div>
+              <div class="item2">
+                <div class="peliculas">Series</div>
+              </div>
+              <div class="item1">
+                <div class="animes">Animes</div>
+              </div>
+              <div class="item1">
+                <div class="animes">Ayuda</div>
+              </div>
+              <div class="item1">
+                <div class="acerca-de">
+                  <span>Acerca</span>
+                  <span class="span"> </span>
+                  <span>de</span>
+                </div>
+              </div>
+              <div class="item14">
+                <div class="buscar">Buscar</div>
+              </div>
+            </div>
+            <img
+              class="logo-lucecine-1"
+              alt=""
+              src="./public/logo-lucecine-1@2x.png"
+            />
+          </div>
+        </div>
+        <div class="body">
+          <b class="recomendaciones">RECOMENDACIONES</b>
+          <div class="rectangle-parent">
+            <img class="frame-child" alt="" src="./public/rectangle-1@2x.png" />
+
+            <div class="titulo-y-sipnosis-de-la-pelicu">
+              <div class="lorem-ipsum-dolor-container">
+                <p class="blank-line">&nbsp;</p>
+                <p class="blank-line">&nbsp;</p>
+                <p class="blank-line">
+                  <b>Lorem ipsum </b>
+                </p>
+                <p class="blank-line">
+                  dolor sit amet, consectetur adipiscing elit. Nullam eu leo
+                  dignissim, rutrum ante sagittis, hendrerit lorem. Phasellus
+                  sed pellentesque augue. Nunc iaculis tellus eget velit dapibus
+                  suscipit. Nam eget vestibulum libero, ut consequat tortor.
+                  Morbi convallis mattis tortor, eu scelerisque nisl rutrum a.
+                  Nullam molestie nisi id suscipit gravida. Quisque pulvinar
+                  sapien vitae faucibus porta. Nullam sit amet luctus eros. Duis
+                  vehicula mollis vestibulum. Nullam tristique magna nec
+                  accumsan sodales. Curabitur tellus est, luctus at sapien eget,
+                  venenatis tincidunt lacus. Sed non erat gravida, consequat
+                  nibh dignissim, vehicula nulla. Mauris purus diam, pharetra id
+                  risus eu, pulvinar lobortis neque. Vivamus facilisis et quam
+                  et elementum.
+                </p>
+                <p class="blank-line">
+                  Nullam nec molestie augue, sed lacinia nunc. Quisque sit amet
+                  enim non turpis gravida vestibulum. Curabitur congue ligula
+                  tristique, sagittis est sit amet, efficitur quam. Mauris
+                  condimentum lorem id orci auctor, a elementum orci dapibus. Ut
+                  porttitor lobortis placerat. Proin sit amet elit blandit,
+                  facilisis velit quis, auctor ante. Vestibulum eu cursus risus,
+                  vitae tincidunt neque. Curabitur lacinia lacus sed bibendum
+                  eleifend. Cras volutpat ex a tempor viverra. In sed odio in
+                  neque suscipit placerat vel ut purus. Sed eu augue a magna
+                  mattis suscipit. Morbi ut consectetur orci. Fusce molestie
+                  fringilla tortor quis dictum.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="rectangle-group">
+            <img class="frame-item" alt="" src="./public/rectangle-44@2x.png" />
+
+            <img class="frame-item" alt="" src="./public/rectangle-45@2x.png" />
+
+            <img class="frame-item" alt="" src="./public/rectangle-46@2x.png" />
+
+            <img class="frame-item" alt="" src="./public/rectangle-47@2x.png" />
+
+            <img class="frame-item" alt="" src="./public/rectangle-48@2x.png" />
+
+            <img class="frame-item" alt="" src="./public/rectangle-49@2x.png" />
+          </div>
+          <b class="peliculas1">PELICULAS</b>
+          <div class="rectangle-container">
+            <img
+              class="frame-child"
+              alt=""
+              src="./public/rectangle-11@2x.png"
+            />
+
+            <div class="titulo-y-sipnosis-de-la-pelicu">
+              <div class="lorem-ipsum-dolor-container">
+                <p class="blank-line">&nbsp;</p>
+                <p class="blank-line">&nbsp;</p>
+                <p class="blank-line">
+                  <b>Lorem ipsum </b>
+                </p>
+                <p class="blank-line">
+                  dolor sit amet, consectetur adipiscing elit. Nullam eu leo
+                  dignissim, rutrum ante sagittis, hendrerit lorem. Phasellus
+                  sed pellentesque augue. Nunc iaculis tellus eget velit dapibus
+                  suscipit. Nam eget vestibulum libero, ut consequat tortor.
+                  Morbi convallis mattis tortor, eu scelerisque nisl rutrum a.
+                  Nullam molestie nisi id suscipit gravida. Quisque pulvinar
+                  sapien vitae faucibus porta. Nullam sit amet luctus eros. Duis
+                  vehicula mollis vestibulum. Nullam tristique magna nec
+                  accumsan sodales. Curabitur tellus est, luctus at sapien eget,
+                  venenatis tincidunt lacus. Sed non erat gravida, consequat
+                  nibh dignissim, vehicula nulla. Mauris purus diam, pharetra id
+                  risus eu, pulvinar lobortis neque. Vivamus facilisis et quam
+                  et elementum.
+                </p>
+                <p class="blank-line">
+                  Nullam nec molestie augue, sed lacinia nunc. Quisque sit amet
+                  enim non turpis gravida vestibulum. Curabitur congue ligula
+                  tristique, sagittis est sit amet, efficitur quam. Mauris
+                  condimentum lorem id orci auctor, a elementum orci dapibus. Ut
+                  porttitor lobortis placerat. Proin sit amet elit blandit,
+                  facilisis velit quis, auctor ante. Vestibulum eu cursus risus,
+                  vitae tincidunt neque. Curabitur lacinia lacus sed bibendum
+                  eleifend. Cras volutpat ex a tempor viverra. In sed odio in
+                  neque suscipit placerat vel ut purus. Sed eu augue a magna
+                  mattis suscipit. Morbi ut consectetur orci. Fusce molestie
+                  fringilla tortor quis dictum.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="frame-div">
+            <img
+              class="frame-item"
+              alt=""
+              src="./public/rectangle-441@2x.png"
+            />
+
+            <img
+              class="frame-item"
+              alt=""
+              src="./public/rectangle-451@2x.png"
+            />
+
+            <img
+              class="frame-item"
+              alt=""
+              src="./public/rectangle-461@2x.png"
+            />
+
+            <img
+              class="frame-item"
+              alt=""
+              src="./public/rectangle-471@2x.png"
+            />
+
+            <img
+              class="frame-item"
+              alt=""
+              src="./public/rectangle-481@2x.png"
+            />
+
+            <img
+              class="frame-item"
+              alt=""
+              src="./public/rectangle-491@2x.png"
+            />
+          </div>
+          <b class="series1">
+            <p class="blank-line">SERIES</p>
+          </b>
+          <div class="rectangle-container">
+            <img
+              class="frame-child"
+              alt=""
+              src="./public/rectangle-12@2x.png"
+            />
+
+            <div class="titulo-y-sipnosis-de-la-pelicu">
+              <div class="lorem-ipsum-dolor-container">
+                <p class="blank-line">&nbsp;</p>
+                <p class="blank-line">&nbsp;</p>
+                <p class="blank-line">
+                  <b>Lorem ipsum </b>
+                </p>
+                <p class="blank-line">
+                  dolor sit amet, consectetur adipiscing elit. Nullam eu leo
+                  dignissim, rutrum ante sagittis, hendrerit lorem. Phasellus
+                  sed pellentesque augue. Nunc iaculis tellus eget velit dapibus
+                  suscipit. Nam eget vestibulum libero, ut consequat tortor.
+                  Morbi convallis mattis tortor, eu scelerisque nisl rutrum a.
+                  Nullam molestie nisi id suscipit gravida. Quisque pulvinar
+                  sapien vitae faucibus porta. Nullam sit amet luctus eros. Duis
+                  vehicula mollis vestibulum. Nullam tristique magna nec
+                  accumsan sodales. Curabitur tellus est, luctus at sapien eget,
+                  venenatis tincidunt lacus. Sed non erat gravida, consequat
+                  nibh dignissim, vehicula nulla. Mauris purus diam, pharetra id
+                  risus eu, pulvinar lobortis neque. Vivamus facilisis et quam
+                  et elementum.
+                </p>
+                <p class="blank-line">
+                  Nullam nec molestie augue, sed lacinia nunc. Quisque sit amet
+                  enim non turpis gravida vestibulum. Curabitur congue ligula
+                  tristique, sagittis est sit amet, efficitur quam. Mauris
+                  condimentum lorem id orci auctor, a elementum orci dapibus. Ut
+                  porttitor lobortis placerat. Proin sit amet elit blandit,
+                  facilisis velit quis, auctor ante. Vestibulum eu cursus risus,
+                  vitae tincidunt neque. Curabitur lacinia lacus sed bibendum
+                  eleifend. Cras volutpat ex a tempor viverra. In sed odio in
+                  neque suscipit placerat vel ut purus. Sed eu augue a magna
+                  mattis suscipit. Morbi ut consectetur orci. Fusce molestie
+                  fringilla tortor quis dictum.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="frame-div">
+            <img
+              class="frame-item"
+              alt=""
+              src="./public/rectangle-442@2x.png"
+            />
+
+            <img
+              class="frame-item"
+              alt=""
+              src="./public/rectangle-452@2x.png"
+            />
+
+            <img
+              class="frame-item"
+              alt=""
+              src="./public/rectangle-462@2x.png"
+            />
+
+            <img
+              class="frame-item"
+              alt=""
+              src="./public/rectangle-472@2x.png"
+            />
+
+            <img
+              class="frame-item"
+              alt=""
+              src="./public/rectangle-482@2x.png"
+            />
+
+            <div class="rectangle-wrapper">
+              <img
+                class="group-child"
+                alt=""
+                src="./public/rectangle-492@2x.png"
+              />
+            </div>
+          </div>
+          <b class="animes1">
+            <p class="blank-line">ANIMES</p>
+          </b>
+          <div class="rectangle-container">
+            <img
+              class="frame-child"
+              alt=""
+              src="./public/rectangle-13@2x.png"
+            />
+
+            <div class="titulo-y-sipnosis-de-la-pelicu">
+              <div class="lorem-ipsum-dolor-container">
+                <p class="blank-line">&nbsp;</p>
+                <p class="blank-line">&nbsp;</p>
+                <p class="blank-line">
+                  <b>Lorem ipsum </b>
+                </p>
+                <p class="blank-line">
+                  dolor sit amet, consectetur adipiscing elit. Nullam eu leo
+                  dignissim, rutrum ante sagittis, hendrerit lorem. Phasellus
+                  sed pellentesque augue. Nunc iaculis tellus eget velit dapibus
+                  suscipit. Nam eget vestibulum libero, ut consequat tortor.
+                  Morbi convallis mattis tortor, eu scelerisque nisl rutrum a.
+                  Nullam molestie nisi id suscipit gravida. Quisque pulvinar
+                  sapien vitae faucibus porta. Nullam sit amet luctus eros. Duis
+                  vehicula mollis vestibulum. Nullam tristique magna nec
+                  accumsan sodales. Curabitur tellus est, luctus at sapien eget,
+                  venenatis tincidunt lacus. Sed non erat gravida, consequat
+                  nibh dignissim, vehicula nulla. Mauris purus diam, pharetra id
+                  risus eu, pulvinar lobortis neque. Vivamus facilisis et quam
+                  et elementum.
+                </p>
+                <p class="blank-line">
+                  Nullam nec molestie augue, sed lacinia nunc. Quisque sit amet
+                  enim non turpis gravida vestibulum. Curabitur congue ligula
+                  tristique, sagittis est sit amet, efficitur quam. Mauris
+                  condimentum lorem id orci auctor, a elementum orci dapibus. Ut
+                  porttitor lobortis placerat. Proin sit amet elit blandit,
+                  facilisis velit quis, auctor ante. Vestibulum eu cursus risus,
+                  vitae tincidunt neque. Curabitur lacinia lacus sed bibendum
+                  eleifend. Cras volutpat ex a tempor viverra. In sed odio in
+                  neque suscipit placerat vel ut purus. Sed eu augue a magna
+                  mattis suscipit. Morbi ut consectetur orci. Fusce molestie
+                  fringilla tortor quis dictum.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="frame-div">
+            <img
+              class="frame-item"
+              alt=""
+              src="./public/rectangle-443@2x.png"
+            />
+
+            <img
+              class="frame-item"
+              alt=""
+              src="./public/rectangle-453@2x.png"
+            />
+
+            <img
+              class="frame-item"
+              alt=""
+              src="./public/rectangle-463@2x.png"
+            />
+
+            <img
+              class="frame-item"
+              alt=""
+              src="./public/rectangle-473@2x.png"
+            />
+
+            <img
+              class="frame-child22"
+              alt=""
+              src="./public/rectangle-483@2x.png"
+            />
+
+            <img
+              class="frame-child23"
+              alt=""
+              src="./public/rectangle-493@2x.png"
+            />
+          </div>
+        </div>
+        <div class="header">
+          <div class="footer1">
+            <div class="siguenos">
+              <div class="siguenos-en-nuestras">Siguenos en nuestras redes</div>
+              <div class="redes">
+                <img
+                  class="instagram-icon"
+                  alt=""
+                  src="./public/instagram.svg"
+                />
+
+                <img
+                  class="instagram-icon"
+                  alt=""
+                  src="./public/facebook.svg"
+                />
+
+                <img class="instagram-icon" alt="" src="./public/tiktok.svg" />
+              </div>
+            </div>
+            <div class="minimenu">
+              <div class="sobre-nosotros">Sobre Nosotros</div>
+              <div class="contacto">Contacto</div>
+              <div class="cuenta">Cuenta</div>
+              <div class="trminos-legales">Términos Legales</div>
+              <div class="ayuda1">Ayuda</div>
+              <div class="sobre-nosotros">Subscripciones</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
