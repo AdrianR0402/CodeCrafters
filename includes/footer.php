4697f94b2d80963b37705091577bf6149d17b28a
IvanRequena <ivanrequena1@gmail.com>
2023-11-06 11:15:20 +0100

    footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,35 +1,26 @@
 <footer class="footer text-center">
         <div class="container">
             <div class="row">
-                <!-- Footer Location-->
-                <div class="col-lg-4 mb-5 mb-lg-0">
-                    <h4 class="text-uppercase mb-4">Location</h4>
-                    <p class="lead mb-0">
-                        2215 John Daniel Drive
-                        <br />
-                        Clark, MO 65243
-                    </p>
-                </div>
+                
                 <!-- Footer Social Icons-->
-                <div class="col-lg-4 mb-5 mb-lg-0">
-                    <h4 class="text-uppercase mb-4">Around the Web</h4>
-                    <a class="btn btn-outline-light btn-social mx-1" href="#!"><i
-                            class="fab fa-fw fa-facebook-f"></i></a>
+                <div class="col-lg-6 mb-5 mb-lg-0 mx-auto">
+                    <h4 class="text-uppercase mb-4">Siguenos en nuestras redes</h4>
+                    <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-facebook-f"></i></a>
                     <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-twitter"></i></a>
-                    <a class="btn btn-outline-light btn-social mx-1" href="#!"><i
-                            class="fab fa-fw fa-linkedin-in"></i></a>
+                    <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-linkedin-in"></i></a>
                     <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-dribbble"></i></a>
                 </div>
-                <!-- Footer About Text-->
-                <div class="col-lg-4">
-                    <h4 class="text-uppercase mb-4">About Freelancer</h4>
-                    <p class="lead mb-0">
-                        Freelance is a free to use, MIT licensed Bootstrap theme created by
-                        <a href="http://startbootstrap.com">Start Bootstrap</a>
-                        .
-                    </p>
+                <div class="row">
+                    <div class="col-lg-10 mb-5 mb-lg-0 mx-auto mt-4">
+                        <a href="#">Sobre Nosotros</a>
+                        <a href="#">Contacto</a>
+                        <a href="#">Cuenta</a>
+                        <a href="#">Términos Legales</b>
+                        <a href="#">Ayuda</b>
+                        <a href="#">Subscripciones</b>
+                    </div>
                 </div>
             </div>
         </div>
-    </footer>
+</footer>
 
