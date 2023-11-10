c3e97ece0824a08b163cf8e14973ce4216d49dc5
jperbel01 <jperbel01@iesmarquesdecomares.org>
2023-11-10 11:31:24 +0100

    creacion peliculas-series.php

---
+++ peliculas-series.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include("includes/head-tag-contents.php"); ?>
+    </head>
+    <body>
+        <?php include("includes/navigation.php"); ?>
+        <header>
+            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="https://hips.hearstapps.com/es.h-cdn.co/fotoes/images/media/imagenes/reportajes/los-posters-latinos-de-peliculas-miticas/catwoman/7124393-1-esl-ES/catwoman.jpg?resize=980:*" class="img-fluid" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="https://hips.hearstapps.com/es.h-cdn.co/fotoes/images/media/imagenes/reportajes/los-posters-latinos-de-peliculas-miticas/desayunocondiamantes/7124363-1-esl-ES/desayunocondiamantes.jpg?resize=980:*" class="img-fluid" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="https://hips.hearstapps.com/es.h-cdn.co/fotoes/images/media/imagenes/reportajes/los-posters-latinos-de-peliculas-miticas/confaldasyaloloco/7124387-1-esl-ES/confaldasyaloloco.jpg?resize=980:*" class="img-fluid" alt="...">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </header>
+        <main>
+            <section class="page-section">
+                <h2>Recomendadas</h2>
+                <div>
+
+                </div>
+            </section>
+        </main>
+        <?php include("includes/footer.php"); ?>
+    </body>
+</html>
