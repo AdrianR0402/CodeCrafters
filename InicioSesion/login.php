c71f4c71b84d4f249fec4359179891a70c796f17
isanito20 <izantrabajoabofi@gmail.com>
2023-11-13 11:17:39 +0100

    añado el heador de texto en inicioSesion.scss y otras cosas

--- InicioSesion/login.php
+++ InicioSesion/login.php
@@ -10,37 +10,35 @@
 
     <?php include("../includes/navigation.php"); ?>
     <main>
-        <section class="page-section inicioSesion" id="inicioSesion">
+        <section class="page-section" id=" inicioSesion">
             <div class="container-fluid my-5">
-                <div class="row">
-                    <div class="col md-4">
-                        oñWDJOJDOIAJD
-                    </div>
-                    <div class="col md-4">
-                        <div class="inicioSesion">
-                            <div class="container-fluid">
-                                <div class="row">
-                                    <div class="col-md-6 offset-md-3">
-                                        <h1 class="text-center">Forgot Password</h1>
-                                        <form>
-                                            <div class="mb-3">
-                                                <label for="email" class="form-label">Email address</label>
-                                                <input type="email" class="form-control" id="email"
-                                                    aria-describedby="emailHelp">
-                                                <div id="emailHelp" class="form-text">We'll never share your email with
-                                                    anyone else.</div>
-                                            </div>
-                                            <button type="submit" class="btn btn-primary">Submit</button>
-                                        </form>
-                                    </div>
+                <div class="col-md-6 mx-auto">
+                    <div class="container">
+                        <div class="row">
+                            <div class="text-center inicioSesion-header">
+                                <h1>INICIAR SESION</h1>
+                            </div>
+
+                            <div class="inicioSesion">
+
+                                <div class="col-md-8 offset-md-2"> <!-- Adjusted size of the column -->
+                                    <form>
+                                        <div class="mb-3">
+                                            <label for="email" class="form-label">Email address</label>
+                                            <input type="email" class="form-control" id="email"
+                                                aria-describedby="emailHelp">
+                                            <div id="emailHelp" class="form-text">We'll never share your email with
+                                                anyone else.</div>
+                                        </div>
+                                        <button type="submit" class="btn btn-primary">Submit</button>
+                                    </form>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="col md-4">
-                        AWIFUHAHFIUH
-                    </div>
                 </div>
+
+            </div>
 
             </div>
         </section>
