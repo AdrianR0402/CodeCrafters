10d2aa591e1d312ebef5fcafbdf238656fb35f53
isanito20 <izantrabajoabofi@gmail.com>
2023-11-14 14:26:57 +0100

    modifico mi inciode sesion.scss y añado un nuevo boton a buttons.scss

--- InicioSesion/login.php
+++ InicioSesion/login.php
@@ -4,42 +4,68 @@
 
 <head>
     <?php include("../includes/head-tag-contents.php"); ?>
+    <link rel="stylesheet" href="tu_estilo_personalizado.css"> <!-- Agrega tu hoja de estilo personalizada aquí -->
+    <style>
+        /* Agrega tu estilo personalizado aquí */
+    </style>
 </head>
 
 <body>
 
     <?php include("../includes/navigation.php"); ?>
     <main>
-        <section class="page-section" id=" inicioSesion">
+
+        <section class="page-section" id="inicioSesion">
             <div class="container-fluid my-5">
                 <div class="col-md-6 mx-auto">
                     <div class="container">
                         <div class="row">
                             <div class="text-center inicioSesion-header">
-                                <h1>INICIAR SESION</h1>
+                                <h1 class="display-4">INICIAR SESIÓN</h1>
+                            </div>
+                            <div class="inicioSesion">
+                                <div class="col-md-8 offset-md-2">
+                                    <form class="mt-4 mb-4">
+                                        <!-- Usuario -->
+                                        <div class="form-group row mb-3">
+                                            <label for="usuario"
+                                                class="col-sm-3 col-form-label text-right font-weight-bold">Usuario:</label>
+                                            <div class="col-sm-9">
+                                                <input type="text" class="form-control" id="usuario">
+                                            </div>
+                                        </div>
+
+                                        <!-- Contraseña -->
+                                        <div class="form-group row mb-4">
+                                            <label for="contrasena"
+                                                class="col-sm-3 col-form-label text-right font-weight-bold">Contraseña:</label>
+                                            <div class="col-sm-9">
+                                                <input type="password" class="form-control" id="contrasena">
+                                            </div>
+                                        </div>
+
+                                        <!-- ¿OLVIDASTE TU CONTRASEÑA? -->
+                                        <div class="form-group row">
+                                            <div class="col-sm-9 offset-sm-3">
+                                                <b class="olvidaste-tu-contrasea font-weight-bold">¿OLVIDASTE TU
+                                                    CONTRASEÑA?</b>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
                             </div>
 
-                            <div class="inicioSesion">
-
-                                <div class="col-md-8 offset-md-2"> <!-- Adjusted size of the column -->
-                                    <form>
-                                        <div class="mb-3">
-                                            <label for="email" class="form-label">Email address</label>
-                                            <input type="email" class="form-control" id="email"
-                                                aria-describedby="emailHelp">
-                                            <div id="emailHelp" class="form-text">We'll never share your email with
-                                                anyone else.</div>
-                                        </div>
-                                        <button type="submit" class="btn btn-primary">Submit</button>
-                                    </form>
+                            <!-- Botón ACCEDER fuera del contenedor -->
+                            <div class="row mt-4">
+                                <div class="col-md-8 offset-md-2">
+                                    <div class="text-center">
+                                        <button type="button" class="btn btn-primary btn-acceder">ACCEDER</button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-
-            </div>
-
             </div>
         </section>
     </main>
