b7acc5d3c7f0113f8f95b82b82de0e4909329291
IvanRequena <ivanrequena1@gmail.com>
2023-11-15 12:11:27 +0100

    forms

--- datosUsuario/password.php
+++ datosUsuario/password.php
@@ -14,21 +14,23 @@
             <div class="container-fluid mt-4 mx-0 px-0">
 
                 <div class="container-fluid m-0 p-0">
-                    <img class="img-fluid bannerDatos" src="/assets/img/bannerPlaceholder.png" alt="">
-                    
+                    <img class="img-fluid bannerDatos" src="/assets/img/bannerPlaceholder1.png" alt="">
                 </div>
+                
                 <div class="container-fluid mt-5 px-5">
                     <div class="container-fluid mx-5 p-5">
                         <div class="row">
                             <div class="col-md-3 text-center">
-                                <h2 class="page-section-heading">General</h2>
-                                <div class="list-group">
-                                    <a href="#" class="list-group-item">Información de Suscripción</a>
-                                    <a href="#" class="list-group-item">Preferencias</a>
-                                    <a href="#" class="list-group-item">Notificaciones por correo</a>
-                                    <a href="#" class="list-group-item">Cambiar correo</a>
-                                    <a href="#" class="list-group-item">Cambiar contraseña</a>
-                                    <a href="#" class="list-group-item">Aplicaciones conectadas</a>
+                                <h2 class="page-section-heading mb-4">General</h2>
+                                <div class="container">
+                                    <ul class="list-group">
+                                        <li class="list-group-item"><a href="#" class="linksMenu">Información de Suscripción</a></li>
+                                        <li class="list-group-item"><a href="#" class="linksMenu">Preferencias</a></li>
+                                        <li class="list-group-item"><a href="#" class="linksMenu">Notificaciones por correo</a></li>
+                                        <li class="list-group-item"><a href="#" class="linksMenu">Cambiar correo</a></li>
+                                        <li class="list-group-item"><a href="#" class="linksMenu">Cambiar contraseña</a></li>
+                                        <li class="list-group-item"><a href="#" class="linksMenu">Aplicaciones conectadas</a></li>
+                                    </ul>
                                 </div>
                             </div>
                             <div class="col-md-9 px-5">
@@ -39,11 +41,11 @@
                                                 <h2 class="datosUsuario-header">Cambiar contraseña</h2>
                                                 <p class="datosUsuario-text">Escoge una contraseña única para mantener segura tu cuenta</p>
                                             </div>
-                                            <div class="col-md-3"></div>
-                                            <div class="col-md-6 text-center">
+                                            <div class="col-md-6 text-center mx-auto">
                                                 <form action="">
-                                                <div class="mb-3 mt-3">
-                                                    <input type="text" class="form-control" id="passAct" placeholder="Contraseña actual" name="passAct">
+                                                <div class="mb-3 mt-3 form-floating">
+                                                    <input type="password" class="form-control" id="passAct" placeholder="Contraseña actual" name="passAct">
+                                                    <label for="passAct">Contraseña actual</label>
                                                 </div>
                                                 <div class="mb-3">
                                                     <input type="password" class="form-control" id="passNueva" placeholder="Nueva contraseña" name="passNueva">
@@ -54,7 +56,6 @@
                                                 <button type="submit" class="btn btn-primary">Cambiar contraseña</button>
                                                 </form>
                                             </div>
-                                            <div class="col-md-3"></div>
                                         </div>
                                     </div>
                                 </div>
