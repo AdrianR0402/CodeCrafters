821a03207e69d88a48268cd1525f9d542f914946
isanito20 <izantrabajoabofi@gmail.com>
2023-11-15 19:43:08 +0100

    modificaciones acerca de

--- acercaDe.php
+++ acercaDe.php
@@ -10,6 +10,11 @@
             max-width: 100%;
             max-height: 100%;
         }
+
+        /* Aplicar negrita a los elementos de texto específicos */
+        .text-bold {
+            font-weight: bold;
+        }
     </style>
 </head>
 
@@ -19,7 +24,7 @@
     <main>
         <section class="page-section" id="inicioSesion">
             <div class="container-fluid py-4 text-center">
-                <h1 class="display-1 text-primary">Lucecine</h1>
+                <h1 class="display-1 text-primary text-bold">Lucecine</h1>
                 <p class="lead text-white">Tu sitio de películas y series favorito</p>
             </div>
 
@@ -27,9 +32,10 @@
             <div class="container mt-4">
                 <p class="text-center text-white" style="font-size: 24px;">Bienvenido a Lucecine, el lugar perfecto para
                     disfrutar de tus
-                    películas y series
+                    <span class="text-bold">películas</span> y <span class="text-bold">series</span>
                     favoritas. Explora nuestro amplio catálogo y sumérgete en el emocionante mundo del cine y la
-                    televisión.</p>
+                    televisión.
+                </p>
             </div>
 
             <!-- Sección de Imágenes con Texto -->
@@ -39,14 +45,16 @@
                     <div class="col-md-6">
                         <img src="assets/img/portfolio/peliculas.png" class="img-fluid img-thumbnail d-block mx-auto"
                             alt="Primera Imagen">
-                        <p class="mt-2 text-center text-white" style="font-size: 20px;">Peliculas
+                        <p class="mt-2 text-center text-white" style="font-size: 60px;">
+                            <span class="text-bold">Peliculas</span>
                         </p>
                     </div>
                     <!-- Segunda Imagen con Texto -->
                     <div class="col-md-6">
                         <img src="assets/img/portfolio/series.png" class="img-fluid img-thumbnail d-block mx-auto"
                             alt="Segunda Imagen">
-                        <p class="mt-2 text-center text-white" style="font-size: 20px;">Series
+                        <p class="mt-2 text-center text-white" style="font-size: 60px;">
+                            <span class="text-bold">Series</span>
                         </p>
                     </div>
                 </div>
@@ -55,14 +63,16 @@
                     <div class="col-md-6">
                         <img src="assets/img/portfolio/animes.png" class="img-fluid img-thumbnail d-block mx-auto"
                             alt="Tercera Imagen">
-                        <p class="mt-2 text-center text-white" style="font-size: 20px;">Animes
+                        <p class="mt-2 text-center text-white" style="font-size: 60px;">
+                            <span class="text-bold">Animes</span>
                         </p>
                     </div>
                     <!-- Cuarta Imagen con Texto -->
                     <div class="col-md-6">
                         <img src="assets/img/portfolio/trailers.png" class="img-fluid img-thumbnail d-block mx-auto"
                             alt="Cuarta Imagen">
-                        <p class="mt-2 text-center text-white" style="font-size: 20px;">Trailers
+                        <p class="mt-2 text-center text-white" style="font-size: 60px;">
+                            <span class="text-bold">Trailers</span>
                         </p>
                     </div>
                 </div>
