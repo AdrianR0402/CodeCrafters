ad388be6efc1217120ee8cd96602242ce9a7fe5e
JBelt02 <granjuan02@gmail.com>
2023-11-16 17:42:31 +0100

    imagenes con buen tamaño

--- peliculas-series.php
+++ peliculas-series.php
@@ -9,27 +9,27 @@
 <body class="series-Peliculas">
     <?php include("includes/navigation.php"); ?>
     <header>
-        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+        <div id="carouselExampleRide" class="carousel slide mx-auto text-center " data-bs-ride="carousel" data-bs-interval="3000">
             <div class="carousel-inner">
-                <div class="carousel-item active carrusel"><!--pon height 200px y d-block w-100-->
-                    <img src="https://hips.hearstapps.com/es.h-cdn.co/fotoes/images/media/imagenes/reportajes/los-posters-latinos-de-peliculas-miticas/catwoman/7124393-1-esl-ES/catwoman.jpg?resize=980:*"
-                        class="img-fluid" alt="...">
+                <div class="carousel-item active "><!--pon height 200px y d-block w-100-->
+                    <img src="assets/img/banner1.jpg"
+                        class="d-block w-100 fixed-size-image" style="height: 800px" alt="imagen1">
                 </div>
-                <div class="carousel-item carrusel">
-                    <img src="https://hips.hearstapps.com/es.h-cdn.co/fotoes/images/media/imagenes/reportajes/los-posters-latinos-de-peliculas-miticas/desayunocondiamantes/7124363-1-esl-ES/desayunocondiamantes.jpg?resize=980:*"
-                        class="img-fluid" alt="...">
+                <div class="carousel-item ">
+                    <img src="assets/img/banner2.webp"
+                        class="d-block w-100 fixed-size-image" style="height: 800px" alt="imagen2">
                 </div>
-                <div class="carousel-item carrusel">
-                    <img src="https://hips.hearstapps.com/es.h-cdn.co/fotoes/images/media/imagenes/reportajes/los-posters-latinos-de-peliculas-miticas/confaldasyaloloco/7124387-1-esl-ES/confaldasyaloloco.jpg?resize=980:*"
-                        class="img-fluid" alt="...">
+                <div class="carousel-item ">
+                    <img src="assets/img/banner3.jpg"
+                        class="d-block w-100 fixed-size-image" style="height: 800px" alt="imagen3">
                 </div>
             </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
+            <button class="carousel-control-prev visually-hidden" type="button" data-bs-target="#carouselExampleRide"
                 data-bs-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Previous</span>
             </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
+            <button class="carousel-control-next visually-hidden" type="button" data-bs-target="#carouselExampleRide"
                 data-bs-slide="next">
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Next</span>
@@ -37,16 +37,16 @@
         </div>
     </header>
     <main>
-        <section class="page-section series-Peliculas " id="series-Peliculas">
+        <section class="series-Peliculas " id="series-Peliculas">
             <div class="container-fluid mt-5 ">
 
-                <h2 class="mb-4">Carteles de Películas</h2>
+                <h2 class="mb-4">Recomendaciones</h2>
 
-                <div class="d-flex overflow-x" style="white-space: nowrap;">
+                <div class="d-flex " style="white-space: nowrap;">
                     <div>
-                        <div class="col-md-4">
-                            <img src="assets/img/background-color4@2x.png" class="float-left mx-auto  img-thumbnail"
-                                alt="Primera Imagen">
+                        <div class="col-md-6" style="margin-right: 20px;">
+                            <img src="assets/img/background-color4@2x.png" class="img-thumbnail"
+                                alt="Primera Imagen" style="max-width: none; height: auto;width: 300px; margin-right: 10px; ">
                             <p class="mt-2 text-white" style="font-size: 30px;">
                                 <span class="text-bold">Titulo Pelicula 1</span>
                             </p>
@@ -55,9 +55,9 @@
 
 
                     <div class="">
-                        <div class="col-md-4">
-                            <img src="assets/img/background-color3@2x.png" class="float-left  mx-auto  img-thumbnail"
-                                alt="Primera Imagen">
+                        <div class="col-md-6" style="margin-right: 20px;">
+                            <img src="assets/img/background-color3@2x.png" class="img-thumbnail"
+                                alt="Primera Imagen" style="max-width: none; height: auto;width: 300px; margin-right: 10px; ">
                             <p class="mt-2 text-white" style="font-size: 30px;">
                                 <span class="text-bold">Titulo Pelicula 1</span>
                             </p>
@@ -65,42 +65,70 @@
                     </div>
 
                     <div class="">
-                        <div class="col-md-4">
+                        <div class="col-md-6" style="margin-right: 20px;">
                             <img src="assets/img/background-color5@2x.png" class="float-left  mx-auto  img-thumbnail"
-                                alt="Primera Imagen">
+                                alt="Primera Imagen" style="max-width: none; height: auto;width: 300px; margin-right: 10px; ">
                             <p class="mt-2 text-white" style="font-size: 30px;">
                                 <span class="text-bold">Titulo Pelicula 1</span>
                             </p>
                         </div>
                     </div>
                     <div class="">
-                        <div class="col-md-4">
+                        <div class="col-md-6" style="margin-right: 20px;">
                             <img src="assets/img/background-color7@2x.png" class="float-left  mx-auto  img-thumbnail"
-                                alt="Primera Imagen">
+                                alt="Primera Imagen" style="max-width: none; height: auto;width: 300px; margin-right: 10px; ">
                             <p class="mt-2 text-white" style="font-size: 30px;">
                                 <span class="text-bold">Titulo Pelicula 1</span>
                             </p>
                         </div>
                     </div>
                     <div class="">
-                        <div class="col-md-4">
+                        <div class="col-md-6" style="margin-right: 20px;">
                             <img src="assets/img/background-color8@2x.png" class="float-left  mx-auto  img-thumbnail"
-                                alt="Primera Imagen">
+                                alt="Primera Imagen" style="max-width: none; height: auto;width: 300px; margin-right: 10px; ">
                             <p class="mt-2 text-white" style="font-size: 30px;">
                                 <span class="text-bold">Titulo Pelicula 1</span>
                             </p>
                         </div>
                     </div>
                     <div class="">
-                        <div class="col-md-4">
+                        <div class="col-md-6" style="margin-right: 20px;">
                             <img src="assets/img/background-color9@2x.png" class="float-left  mx-auto  img-thumbnail"
-                                alt="Primera Imagen">
+                                alt="Primera Imagen" style="max-width: none; height: auto;width: 300px; margin-right: 10px; ">
+                            <p class="mt-2 text-white" style="font-size: 30px;">
+                                <span class="text-bold">Titulo Pelicula 1</span>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="">
+                        <div class="col-md-6" style="margin-right: 20px;">
+                            <img src="assets/img/background-color9@2x.png" class="float-left  mx-auto  img-thumbnail"
+                                alt="Primera Imagen" style="max-width: none; height: auto;width: 300px; margin-right: 10px; ">
+                            <p class="mt-2 text-white" style="font-size: 30px;">
+                                <span class="text-bold">Titulo Pelicula 1</span>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="">
+                        <div class="col-md-6" style="margin-right: 20px;">
+                            <img src="assets/img/background-color9@2x.png" class="float-left  mx-auto  img-thumbnail"
+                                alt="Primera Imagen" style="max-width: none; height: auto;width: 300px; margin-right: 10px; ">
+                            <p class="mt-2 text-white" style="font-size: 30px;">
+                                <span class="text-bold">Titulo Pelicula 1</span>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="">
+                        <div class="col-md-6" style="margin-right: 20px;">
+                            <img src="assets/img/background-color9@2x.png" class="float-left  mx-auto  img-thumbnail"
+                                alt="Primera Imagen" style="max-width: none; height: auto;width: 300px; margin-right: 10px; ">
                             <p class="mt-2 text-white" style="font-size: 30px;">
                                 <span class="text-bold">Titulo Pelicula 1</span>
                             </p>
                         </div>
                     </div>
                 </div>
+            </div>
         </section>
     </main>
     <?php include("includes/footer.php"); ?>
