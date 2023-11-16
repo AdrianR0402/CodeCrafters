7c6825ded0232af4ec47225312af6bceacf7ec00
JBelt02 <granjuan02@gmail.com>
2023-11-16 18:51:16 +0100

    cambiar el nombre de la ventana emergente

--- peliculas-series.php
+++ peliculas-series.php
@@ -359,7 +359,7 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title">Contenido Emergente</h5>
+                    <h5 class="modal-title">Caracteristicas</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
