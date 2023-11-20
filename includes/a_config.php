ce1aede2363eab93888d68b0bc3df2450104853f
isanito20 <izantrabajoabofi@gmail.com>
2023-11-20 10:37:19 +0100

    comentarios en todos los includes

--- includes/a_config.php
+++ includes/a_config.php
@@ -1,47 +1,47 @@
 <?php
-	switch ($_SERVER["SCRIPT_NAME"]) {
-		case "/index.php":
-			$CURRENT_PAGE = "Index"; 
-			$PAGE_TITLE = "Lucecine";
-			break;
-		case "/acercaDe.php":
-			$CURRENT_PAGE = "acercaDe"; 
-			$PAGE_TITLE = "Sobre nosotros";
-			break;
-		case "/contact.php":
-			$CURRENT_PAGE = "Contact"; 
-			$PAGE_TITLE = "Contáctanos";
-			break;
-		case "/password.php";
-			$CURRENT_PAGE = "Password"; 
-			$PAGE_TITLE = "Datos de Usuario";
-			break;
-		case "/notifications.php";
-			$CURRENT_PAGE = "notifications"; 
-			$PAGE_TITLE = "Datos de Usuario";
-			break;
-		case "/preferencias.php";
-			$CURRENT_PAGE = "preferencias"; 
-			$PAGE_TITLE = "Datos de Usuario";
-			break;
-		case "/infoSuscripcion.php";
-			$CURRENT_PAGE = "infoSus"; 
-			$PAGE_TITLE = "Datos de Usuario";
-			break;
-		case "/cambiarCorreo.php";
-			$CURRENT_PAGE = "correo"; 
-			$PAGE_TITLE = "Datos de Usuario";
-			break;
-		case "/acercaDe.php";
-			$CURRENT_PAGE = "acercaDe"; 
-			$PAGE_TITLE = "Acerca de";
-			break;
-		case "/ayuda.php";
-			$CURRENT_PAGE = "ayuda"; 
-			$PAGE_TITLE = "Ayuda";
-			break;
-		default:
-			$CURRENT_PAGE = "";
-			$PAGE_TITLE = "Lucecine";
-	}
+switch ($_SERVER["SCRIPT_NAME"]) {
+	case "/index.php":
+		$CURRENT_PAGE = "Index";
+		$PAGE_TITLE = "Lucecine";
+		break;
+	case "/acercaDe.php":
+		$CURRENT_PAGE = "acercaDe";
+		$PAGE_TITLE = "Sobre nosotros";
+		break;
+	case "/contact.php":
+		$CURRENT_PAGE = "Contact";
+		$PAGE_TITLE = "Contáctanos";
+		break;
+	case "/password.php";
+		$CURRENT_PAGE = "Password";
+		$PAGE_TITLE = "Datos de Usuario"; //añadir las diferentes partes de la web
+		break;
+	case "/notifications.php";
+		$CURRENT_PAGE = "notifications";
+		$PAGE_TITLE = "Datos de Usuario";
+		break;
+	case "/preferencias.php";
+		$CURRENT_PAGE = "preferencias";
+		$PAGE_TITLE = "Datos de Usuario";
+		break;
+	case "/infoSuscripcion.php";
+		$CURRENT_PAGE = "infoSus";
+		$PAGE_TITLE = "Datos de Usuario";
+		break;
+	case "/cambiarCorreo.php";
+		$CURRENT_PAGE = "correo";
+		$PAGE_TITLE = "Datos de Usuario";
+		break;
+	case "/acercaDe.php";
+		$CURRENT_PAGE = "acercaDe";
+		$PAGE_TITLE = "Acerca de";
+		break;
+	case "/ayuda.php";
+		$CURRENT_PAGE = "ayuda";
+		$PAGE_TITLE = "Ayuda";
+		break;
+	default:
+		$CURRENT_PAGE = "";
+		$PAGE_TITLE = "Lucecine";
+}
 ?>
--- includes/footer.php
+++ includes/footer.php
@@ -6,7 +6,8 @@
             <div class="col-lg-6 mb-5 mb-lg-0 mx-auto">
                 <h4 class="text-uppercase mb-4">Siguenos en nuestras redes</h4>
                 <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-facebook-f"></i></a>
-                <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-twitter"></i></a>
+                <a class="btn btn-outline-light btn-social mx-1" href="#!"><i
+                        class="fab fa-fw fa-twitter"></i></a><!--iconos de redes sociales -->
                 <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-linkedin-in"></i></a>
                 <a class="btn btn-outline-light btn-social mx-1" href="#!"><i class="fab fa-fw fa-dribbble"></i></a>
             </div>
@@ -15,7 +16,7 @@
             <div class="col-lg-10 mb-0 mb-lg-0 mx-auto mt-4">
                 <a href="acercaDe.php">Sobre Nosotros</a>
                 <a href="contact.php">Contacto</a>
-                <a href="infoSuscripcion.php">Cuenta</a>
+                <a href="infoSuscripcion.php">Cuenta</a><!--enlaces a diferentes partes de la web -->
                 <a href="terminosLegales.php">Términos Legales</a>
                 <a href="ayuda.php">Ayuda</a>
                 <a href="infoSuscripcion.php">Subscripciones</a>
