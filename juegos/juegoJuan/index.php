9c5abcc14c466c2f237b0227a84cd9ea666ae4ff
jperbel01 <jperbel01@iesmarquesdecomares.org>
2023-12-18 11:18:48 +0100

    estilo juego juan casi acabado (falta responsivo)

--- juegos/juegoJuan/index.php
+++ juegos/juegoJuan/index.php
@@ -1,8 +1,8 @@
-<?php include("/includes/a_config.php"); ?>
+<?php include("../../includes/a_config.php"); ?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
-    <?php include("/includes/head-tag-contents.php"); ?>
+    <?php include("../../includes/head-tag-contents.php"); ?>
   <style>
     .custom-grid {
       display: grid;
@@ -14,60 +14,83 @@
     .square {
       height: 200px;
       width: 200px;
-      border: solid black 1px;
+      border: solid white 1px;
       background-size: cover;
+      background-color:white;
     }
-
-    
 
     /* Estilos adicionales para el diseño con Bootstrap */
-    body {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      flex-direction: column;
-      height: 100vh;
-    }
-
     h1, h2 {
       text-align: center;
       margin-bottom: 20px;
+      color: white;
+    }
+    .leaderboard-section {
+            padding-top: 1px; 
+            padding-bottom: 50px; 
+        }
+    .main-section {
+          padding-top: 170px; 
+          padding-bottom: 50px; 
+          background-image: url("../../assets/img/portfolio/fondojuego1.jpg");
     }
   </style>
 </head>
 
 <body>
-  <?php include("/includes/navigation.php"); ?>
-  <main>
-    <div class="container">
-      <h1>Dale a Todas las Películas que puedas</h1>
-      <div class="d-flex justify-content-center mb-4">
-        <div class="col-4">
-          <h2>Tu marcador:</h2>
-          <h2 id="score">0</h2>
+  <?php include("../../includes/navigation.php"); ?>
+  
+    <section class="page-section main-section">
+      <div class="container">
+        <h1>Dale a Todas las Películas que puedas</h1><br><br>
+        <div class="d-flex justify-content-center mb-4">
+          <div class="col-4">
+            <h2>Tu marcador:</h2>
+            <h2 id="score">0</h2>
+          </div>
+          <div class="col-4">
+            <h2>Tiempo Restante :</h2>
+            <h2 id="time-left">60</h2>
+          </div>
         </div>
-        <div class="col-4">
-          <h2>Tiempo Restante :</h2>
-          <h2 id="time-left">60</h2>
-        </div>
-      </div>
 
-      <div class="d-flex justify-content-center"></div>
-        <div class="custom-grid ">
-          <div class="square" id="1"></div>
-          <div class="square" id="2"></div>
-          <div class="square" id="3"></div>
-          <div class="square" id="4"></div>
-          <div class="square" id="5"></div>
-          <div class="square" id="6"></div>
-          <div class="square" id="7"></div>
-          <div class="square" id="8"></div>
-          <div class="square" id="9"></div>
+        <div class="d-flex justify-content-center"></div>
+          <div class="custom-grid ">
+            <div class="square" id="1"></div>
+            <div class="square" id="2"></div>
+            <div class="square" id="3"></div>
+            <div class="square" id="4"></div>
+            <div class="square" id="5"></div>
+            <div class="square" id="6"></div>
+            <div class="square" id="7"></div>
+            <div class="square" id="8"></div>
+            <div class="square" id="9"></div>
+          </div>
         </div>
       </div>
-    </div>
-  </main>
-  <?php include("/includes/footer.php"); ?>
+      <section class=" leaderboard-section">
+        <div class="container">
+            <div class="card text-center mt-4">
+                <div class="card-header">
+                    ¡ Mejores Puntuaciones !
+                </div>
+                <div class="card-body">
+                    <!-- Agrega aquí el contenido del leaderboard, puedes usar tablas, listas o cualquier otro elemento según tus necesidades -->
+                    <p class="card-text">Juan: 25</p>
+                    <p class="card-text">Ivan: 33</p>
+                    <p class="card-text">Izan: 12</p>
+                    <!-- Ajusta el contenido según tu necesidad -->
+                </div>
+            </div>
+        </div>
+    </section>
+
+    </section>
+
+    
+
+ 
+  <?php include("../../includes/footer.php"); ?>
   <script src="app.js"></script>
 </body>
 
