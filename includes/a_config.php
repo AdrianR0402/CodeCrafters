7c8b458fb07e5e9da887163838e0311ee9f16620
isanito20 <izantrabajoabofi@gmail.com>
2024-01-12 11:23:46 +0100

    a config y dbconnenction

--- includes/a_config.php
+++ includes/a_config.php
@@ -48,4 +48,30 @@ switch ($_SERVER["SCRIPT_NAME"]) {
 		$CURRENT_PAGE = "";
 		$PAGE_TITLE = "Lucecine";
 }
+//Include Google Client Library for PHP autoload file
+require_once 'vendor/autoload.php';
+
+//Make object of Google API Client for call Google API
+$google_client = new Google_Client();
+
+//Set the OAuth 2.0 Client ID
+$google_client->setClientId('849911634904-h9gtp4id4vqi8fbk94aju4on4mqf4jqa.apps.googleusercontent.com');
+
+//Set the OAuth 2.0 Client Secret key
+$google_client->setClientSecret('nuLzdBqfMNwrmzLb0B4MbmRL');
+
+//Set the OAuth 2.0 Redirect URI
+$google_client->setRedirectUri('http://' . $_SERVER['SERVER_NAME'] . '/index.php');
+
+
+$google_client->addScope('email');
+
+$google_client->addScope('profile');
+
+//start session on web page
+session_start();
+
+$login_button = '';
+
+
 ?>
